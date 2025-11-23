d48b55a28cd207ebd337f41e1c3ee171e1f50a6f
daniloldn <daniloldn25@gmail.com>
2025-11-23 20:02:20 +0000

    data handling

--- backend/5dinterpolator/orchestrator.py
+++ backend/5dinterpolator/orchestrator.py
@@ -1,9 +1,17 @@
-from data_handling import load_data
+from data_handling import load_data, validate_data
 
 
 def process_data(file_path: str):
 
     df = load_data(file_path)
+
+    issues = []
+    if df.empty:
+        issues.append("data set is empty")
+    if not validate_data(df):
+        issues.append("data does not have the right dimension")
+    if df.isna().sum().sum() > 0:
+        issues.append("data has missing values")
 
     #add other processing steps
 
